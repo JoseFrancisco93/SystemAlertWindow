893e14f2e8b6cc6c1c3e3aab42dd649a9cc7ae2f
JoseFrancisco93 <jfvc.93@gmail.com>
2023-06-27 18:10:14 +0000

    last changes

--- ios/Classes/SwiftSystemAlertWindowPlugin.swift
+++ ios/Classes/SwiftSystemAlertWindowPlugin.swift
@@ -2,22 +2,54 @@ import Flutter
 import UIKit
 
 public class SwiftSystemAlertWindowPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "system_alert_window", binaryMessenger: registrar.messenger())
-    let instance = SwiftSystemAlertWindowPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    private var alertController: UIAlertController?
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if (call.method == "getPlatformVersion") {
-               result("iOS " + UIDevice.current.systemVersion)
-           }
-           else if (call.method == "showSystemWindow") {
-               DispatchQueue.main.async {
-                   let alert = UIAlertController(title: "Alert", message: "Hi, My name is flutter", preferredStyle: .alert);
-                   alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-                   UIApplication.shared.keyWindow?.rootViewController?.present(alert, animated: true, completion: nil);
-               }
-           }
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "system_alert_window", binaryMessenger: registrar.messenger())
+        let instance = SwiftSystemAlertWindowPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if call.method == "getPlatformVersion" {
+            result("iOS " + UIDevice.current.systemVersion)
+        } else if call.method == "showSystemWindow" {
+            if let arguments = call.arguments as? [Any], let params = arguments[2] as? [String: Any], let userName = params["userName"] as? String {
+                DispatchQueue.main.async {
+                    self.showSystemWindow(withUserName: userName)
+                }
+            }
+        } else if call.method == "closeSystemWindow" {
+            DispatchQueue.main.async {
+                self.closeSystemWindow()
+            }
+        }
+    }
+
+    private func showSystemWindow(withUserName userName: String) {
+        guard alertController == nil else {
+            return
+        }
+
+        let alert = UIAlertController(title: nil, message: "Llamada con \(userName)", preferredStyle: .alert)
+        alertController = alert
+
+        // Apariencia
+        let windowSubview = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = UIViewController()
+        windowSubview.rootViewController = viewController
+        windowSubview.windowLevel = UIWindow.Level.alert + 1
+        windowSubview.isHidden = false
+
+        viewController.present(alert, animated: true, completion: nil)
+    }
+
+    private func closeSystemWindow() {
+        guard let alert = alertController else {
+            return
+        }
+
+        alertController = nil
+        alert.dismiss(animated: true, completion: nil)
+    }
 }
