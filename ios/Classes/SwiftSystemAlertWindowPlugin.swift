bca7993a78c9d9f9327989f81723f2e9ea109879
JoseFrancisco93 <jfvc.93@gmail.com>
2023-06-29 19:43:16 +0000

    update values

--- ios/Classes/SwiftSystemAlertWindowPlugin.swift
+++ ios/Classes/SwiftSystemAlertWindowPlugin.swift
@@ -12,12 +12,5 @@ public class SwiftSystemAlertWindowPlugin: NSObject, FlutterPlugin {
     if (call.method == "getPlatformVersion") {
                result("iOS " + UIDevice.current.systemVersion)
            }
-           else if (call.method == "showSystemWindow") {
-               DispatchQueue.main.async {
-                   let alert = UIAlertController(title: "Alert", message: "Hi, My name is flutter", preferredStyle: .alert);
-                   alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-                   UIApplication.shared.keyWindow?.rootViewController?.present(alert, animated: true, completion: nil);
-               }
-           }
   }
 }
